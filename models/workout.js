979f153fa144548a233225733814ed0663ee568c
Leah Barnes <johnson.leahe@gmail.com>
2020-02-23 13:21:02 -0500

    updating workout.js virtual.

--- models/workout.js
+++ models/workout.js
@@ -41,65 +41,13 @@ const workoutSchema = new Schema({
       virtuals: true
     }
   });
+  // going through exercises array and performing the reduce method to create total duration (adds all numbers together in sequence)
+  // adds dynamically created property to schema
 workoutSchema.virtual("totalDuration").get(function () {
-  return this.exercises.reduce((total, activity) => {
-    return total + activity.duration;
+  // reduces the array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
   }, 0);
 });
 const Workout = mongoose.model("workout", workoutSchema);
 module.exports = Workout;
-
-// const mongoose = require("mongoose");
-
-// const Schema = mongoose.Schema;
-
-// const WorkoutSchema = new Schema({
-
-//     day: {
-//         type: Date,
-//         default: Date.now
-//     },
-
-//     exercises: [
-//         {
-//             type: {
-//                 type: String,
-//                 trim: true,
-//                 required: "Type of exercise is required."
-//             },
-
-//             name: {
-//                 type: String,
-//                 trim: true,
-//                 required: "Name of exercise is required."
-//             },
-
-//             duration: {
-//                 type: Number,
-//                 required: "Duration (minutes) of exercise is required."
-//             },
-
-//             // following not required because depending on exercise may not be applicable
-//             weight: {
-//                 type: Number,
-//             },
-
-//             distance: {
-//                 type: Number,
-//             },
-
-//             reps: {
-//                 type: Number,
-//             },
-
-//             sets: {
-//                 type: Number,
-//             }
-//         }
-//     ]
-// }
-// );
-
-// const Workout = mongoose.model("Workout", WorkoutSchema);
-
-// module.exports = Workout;
